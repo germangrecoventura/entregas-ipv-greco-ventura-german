e61a19021120232adbcc1cc758785519b3626f0c
germangrecoventura <germangv1998@gmail.com>
2022-09-02 13:22:07 -0300

    Agrego enemigo con su disparo

--- entrega-2-greco-ventura-german/Main.gd
+++ entrega-2-greco-ventura-german/Main.gd
@@ -4,3 +4,4 @@ extends Node
 func _ready():
 	
 	$Player.set_projectile_container(self)
+	$Turret.set_values($Player,self)
--- entrega-2-greco-ventura-german/entities/Projectile.gd
+++ entrega-2-greco-ventura-german/entities/Projectile.gd
@@ -3,6 +3,9 @@ class_name Projectile
 
 var direction:Vector2
 
+signal delete_requested(projectile)
+
+export (float) var speed
 
 func _ready():
 	set_physics_process(false)
@@ -13,4 +16,4 @@ func set_starting_values(starting_position:Vector2, direction:Vector2):
 	set_physics_process(true)
 	
 func _process(delta):
-	position += direction * 50 * delta
+	position += direction * speed * delta
