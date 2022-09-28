876211945842551182dcd68fe3d4938d7e617fa2
germangrecoventura <germangv1998@gmail.com>
2022-09-28 13:10:18 -0300

    Arreglo transición

--- con animacion muerte/entrega-5-greco-ventura-german/enviroment/items/Trophy.gd
+++ con animacion muerte/entrega-5-greco-ventura-german/enviroment/items/Trophy.gd
@@ -1,5 +1,6 @@
 extends Area2D
 
+# warning-ignore:unused_argument
 func _on_Trophy_body_entered(body):
 	$"../../CanvasLayer/AnimationPlayer".play("fade")
 	get_tree().get_nodes_in_group("ui")[0].text = "You Win!"
