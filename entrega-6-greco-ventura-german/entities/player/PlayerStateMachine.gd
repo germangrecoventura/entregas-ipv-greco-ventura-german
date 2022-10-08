9493bab688406ee2c5bb61298b143bbfb875cd71
germangrecoventura <germangv1998@gmail.com>
2022-10-08 20:06:01 -0300

    Agrego daño player

--- entrega-6-greco-ventura-german/entities/player/PlayerStateMachine.gd
+++ entrega-6-greco-ventura-german/entities/player/PlayerStateMachine.gd
@@ -1,5 +1,5 @@
 extends "res://entities/AbstractStateMachine.gd"
-
+onready var player_sfx = $"../PlayerSfx"
 
 func _ready():
 	states_map = {
@@ -12,6 +12,9 @@ func _ready():
 
 
 func notify_hit(amount):
+	var sfx = load("res://assets/audio/sfx/playerHurt.wav")
+	player_sfx.stream = sfx
+	player_sfx.play()
 	PlayerData.current_health += min(amount, PlayerData.max_health)
 	print(PlayerData.current_health)
 	if PlayerData.current_health == 0:
